a11afa6c51a68609fc0490c46f2019e2a739b400
Spandana B <spandanaspandu907@gmail.com>
2026-05-23 22:11:54 +0530

    update

---
+++ Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int a = 10;
+        float b = 5.5f;
+        double c = 99.99;
+        char d = 'A';
+        boolean e = true;
+
+        System.out.println("int value: " + a);
+        System.out.println("float value: " + b);
+        System.out.println("double value: " + c);
+        System.out.println("char value: " + d);
+        System.out.println("boolean value: " + e);
+    }
+}
